d73eb140713ce3b746af065682f5be24c3050d9b
didier <didier@d-defrance.fr>
2021-02-09 09:44:28 +0100

    variantes

--- exemples/xml_dtd/validation/lancer_validation_xml_via_dtd.bat
+++ exemples/xml_dtd/validation/lancer_validation_xml_via_dtd.bat
@@ -6,13 +6,13 @@ set JAVA_HOME=C:\Program Files\Java\jdk-11.0.4
 REM set MyXmlJavaJar="D:\tp\local-git-mycontrib-repositories\tp_xml\parseurs\bin\myJavaXml.jar"
 set MyXmlJavaJar="../../../parseurs/bin/myJavaXml.jar"
 
-set XMLPath=adresse.xml
+REM set XMLPath=adresse.xml
 REM set XMLPath=adresse_v2.xml
 REM set XMLPath=adresse_debug.xml
 REM set XMLPath=adresse_no_debug.xml
 rem set XMLPath=adresseInvalid.xml
 REM set XMLPath=lettre.xml
-REM set XMLPath=lettreInvalid.xml
+set XMLPath=lettreInvalid.xml
 
 rem set OptionalDtdPath=address.dtd
 
